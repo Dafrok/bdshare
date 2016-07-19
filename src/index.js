b513a0bb333f31facef03362da83b85b4f70e3a6
Dafrok <o.o@mug.dog>
2016-07-19 16:21:27 +0800

    add share cmd list

--- src/index.js
+++ src/index.js
@@ -1,33 +1,9 @@
 import $ from 'jquery'
 import init from './init.js'
 import shareDOM from './share-dom/index.js'
+import cmdList from './cmd-list.js'
 
-var $body = $('body');
-var $share = $('<div class="share-btn"></div>').css({display: 'none'});
-var $shareGroup = $('<div class="share-icon-group bdsharebuttonbox"></div>');
-var $weibo = $('<a href="javascript:;" class="bds-btn btn-weibo bds_tsina" data-cmd="tsina"></a>');
-var $qzone = $('<a href="javascript:;" class="bds-btn btn-qzone bds_qzone" data-cmd="qzone"></a>');
-var $wechat = $('<a href="javascript:;" class="bds-btn btn-wechat bds_weixin" data-cmd="weixin"></a>');
-
-const shareList = []
-shareList.push($weibo)
-shareList.push($qzone)
-shareList.push($wechat)
-shareDOM.bind(shareList)
-
+const handlers = shareDOM.bind()
 init()
 
-module.exports = {
-  weibo: function (options) {
-    window._bd_share_config.common = options || window._bd_share_config.common;
-    $weibo[0].click();
-  },
-  wechat: function (options) {
-    window._bd_share_config.common = options || window._bd_share_config.common;
-    $wechat[0].click();
-  },
-  qzone: function (options) {
-    window._bd_share_config.common = options || window._bd_share_config.common;
-    $qzone[0].click();
-  }
-};
+module.exports = handlers
--- src/share-dom/bind.js
+++ src/share-dom/bind.js
@@ -1,8 +1,18 @@
 import $ from 'jquery'
+import cmdList from '../data/cmd-list.js'
 
-$shareGroup.appendTo($share)
-  .append($weibo)
-  .append($qzone)
-  .append($wechat);
-
+const $body = $('body')
+const handlers = {}
+var $share = $('<div class="bdsharebuttonbox"></div>').css({display: 'none'});
+for (var i = 0; i < cmdList.length; i++) {
+    ~function (i) {
+        var $item = $('<a data-cmd="' + cmdList[i] + '"></a>').appendTo($share);
+        handlers[cmdList[i]] = function (options) {
+            window._bd_share_config.common = options || window._bd_share_config.common;
+            $item[0].click();
+        }
+    }(i)
+}
 $share.appendTo($body);
+
+export default handlers
