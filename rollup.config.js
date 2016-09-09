542a3e59093cf09bf042721f97fb49e014f02b7b
Dafrok <o.o@mug.dog>
2016-09-09 15:47:29 +0800

    support es2015-rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,4 @@
 import babel from 'rollup-plugin-babel'
-import nodeResolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import replace from 'rollup-plugin-replace'
 
 export default {
     entry: 'src/index.js',
@@ -9,17 +6,6 @@ export default {
     format: 'umd',
     moduleName: 'bdshare',
     plugins: [
-        // babel({presets: ['es2015-rollup']}),
-        nodeResolve({
-            jsnext: true,
-            main: true,
-            extensions: [ '.js', '.json' ]
-        }),
-        commonjs({
-            include: 'node_modules/**'
-        }),
-        replace({
-            'process.env.NODE_ENV': "'production'"
-        })
+        babel({presets: ['es2015-rollup']})
     ]
 }
